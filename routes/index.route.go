168753e2b4da0f5a64f76cc4f32f6630bda5f3e0
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-11-01 09:47:14 +0700

    add static assets

--- routes/index.route.go
+++ routes/index.route.go
@@ -1,13 +1,14 @@
 package routes
 
 import (
+	"belajar-gofiber-gorm/config"
 	"belajar-gofiber-gorm/controllers"
 
 	"github.com/gofiber/fiber/v2"
-
 )
 
 func RouteInit(r *fiber.App) {
+	r.Static("/public", config.ProjectRootPath+"/public/assets")
 	r.Get("/user", controllers.UserControllerGetAll)
 	r.Get("/user/:id", controllers.UserControllerGetById)
 	r.Post("/user", controllers.UserControllerCreate)
